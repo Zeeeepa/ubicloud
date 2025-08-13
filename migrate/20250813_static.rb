268e327ded090babff7ff7668677279c257f35ea
Hadi Moshayedi <hadi@ubicloud.com>
2025-08-13 03:18:55 -0700

    Adjustments

--- migrate/20250813_static.rb
+++ migrate/20250813_static.rb
@@ -3,7 +3,7 @@
 Sequel.migration do
   change do
     create_table(:static_app) do
-      primary_key :id, type: :uuid
+      column :id, :uuid, primary_key: true
       foreign_key :project_id, :project, type: :uuid, null: false
       String :repository, null: false
       String :branch, null: false
